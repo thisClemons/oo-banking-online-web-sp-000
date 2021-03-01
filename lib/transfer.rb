1fa9c8f3a73c0722e4ec0d4e933c56a9211db8bf
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 22:41:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid? && !Transfer.past_transfers.include?(self)
+    if sender.balance - amount > 0 && !Transfer.past_transfers.include?(self)
       # binding.pry
       sender.deposit(-amount)
       receiver.deposit(amount)
