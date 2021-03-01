f245bf39454b7c3a081a222885ada28c6c687f34
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 22:36:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
     if sender.valid? && !Transfer.past_transfers.include?(self)
       sender.deposit(-amount)
       receiver.deposit(amount)
-      self.status = "complete"
+      status = "complete"
       Transfer.past_transfers << self
     else
       status = "rejected"
